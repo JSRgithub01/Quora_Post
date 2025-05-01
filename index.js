e98aac1ea5522ecaa02a0581a83b11091a6016d2
Akmgithub01 <144653800+Akmgithub01@users.noreply.github.com>
2025-05-01 13:24:52 +0530

    completed my first backend project in node after creating quora post

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 8000;
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 
@@ -11,17 +14,17 @@ app.set("views", path.join(__dirname, "views"));
 
 let posts = [
     {
-        id: "1a",
+        id: uuidv4(),
         username : "AKM01",
         content : "This is post01"
     },
     {
-        id: "2b",
+        id: uuidv4(),
         username : "AKM02",
         content : "This is post02"
     },
     {
-        id: "3c",
+        id: uuidv4(),
         username : "AKM03",
         content : "This is post03"
     }
@@ -37,7 +40,7 @@ app.get("/posts/new", (req,res)=>{
 
 app.post("/posts", (req,res)=>{
     let {username, content} = req.body;
-    posts.push({username,content})
+    posts.push({id: uuidv4(), username,content});
     res.redirect("/posts");
 })
 
@@ -46,8 +49,33 @@ app.get("/posts/:id",(req,res)=>{
     let post = posts.find((p)=>{
         return id === p.id;
     })
+    console.log(id);
+    console.log(post);
     res.render("show.ejs", {post});
 })
+
+app.patch("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content
+    let post = posts.find((p) => p.id === id );
+    post.content = newContent;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit", (req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id );
+    console.log(post);
+    res.render("edit.ejs", {post});
+})
+
+app.delete("/posts/:id", (req,res)=>{
+    let {id} = req.params
+    posts = posts.filter((p)=> p.id != id);
+    console.log(posts);
+    res.redirect("/posts");
+})
+
 
 app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`);
